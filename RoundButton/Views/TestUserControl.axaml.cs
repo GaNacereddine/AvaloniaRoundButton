61c71426b57cf4d48a228b6031289b576fa71987
GNasreddine <GNasreddine@outlook.com>
2021-06-15 22:00:22 +0000

    moved user control to the views folder

--- RoundButton/Views/TestUserControl.axaml.cs
+++ RoundButton/Views/TestUserControl.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace RoundButton.ViewModels
+namespace RoundButton.Views
 {
     public partial class TestUserControl : UserControl
     {
